dc4b7d05abf8022fbe8872477b0bca802e5820fb
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-08-18 13:42:05 +0300

    create some components and update the app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
+import Header from "./components/header";
+import UserInput from "./components/UserInput";
+import Results from "./components/Results";
+import { useState } from "react";
+
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput((prevUserInput) => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      <Results input={userInput} />
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Results.jsx
@@ -0,0 +1,7 @@
+import { calculateInvestmentResults } from "../util/investment";
+
+export default function Results({ input }) {
+  const resultsData = calculateInvestmentResults(input);
+
+  return;
+}

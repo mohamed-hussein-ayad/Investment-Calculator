f2151a23fe1b9887491a20108c3a83f310585038
mohamed ayad <126675322+mohamed-hussein-ayad@users.noreply.github.com>
2024-08-18 14:01:08 +0300

    the app is done and it is waiting for updates

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -24,7 +26,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater then zero</p>
+      )}
+      {inputIsValid && <Results input={userInput} />}
     </>
   );
 }
